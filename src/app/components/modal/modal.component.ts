071bba271d485f3a817a3b2fff954647a8973831
Icaruk <adri-rk@hotmail.com>
2019-11-27 16:30:13 +0100

    Modal detalle peli funcionando

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
 	selector: "app-modal",
@@ -9,17 +10,49 @@ export class ModalComponent implements OnInit {
 	@Input()
 	movie: any;
 	
-	show:boolean
-	constructor() {}
-
-	ngOnInit() {
-		console.log( this.movie );
-	}
-
+	isVisible: boolean = false;
+	constructor(private movieService: MovieService) {}
 	
+	
+	ngOnInit() {
+		// console.log( this.movie );
+	}
 	
 	openDialog() {
-		this.show = true;
-		console.log(this.movie.id);
+		
+		let movie = this.movie;
+		
+		
+		// Preparo la info para enviar al HTML
+		movie.releaseYear = new Date(movie.release_date).getFullYear();
+		movie.strGenres = "";
+		
+		for (let _id of movie.genre_ids) {
+			movie.strGenres += `${this.movieService.idToGenre(_id)} `;
+		};
+		
+		
+		// Muestro
+		this.isVisible = true;
+		
 	}
+	
+	closeDialog(ev) {
+		
+		// He pulsado sobre la X roja (no recibo $event)
+		if (!ev) {
+			this.isVisible = false;
+			return;
+		};
+		
+		
+		// He pulsado sobre algo
+		if (ev.target.id) {
+			if (ev.target.id === "fondoModal") { // ese algo, es el fondo
+				this.isVisible = false;
+			};
+		};
+		
+	}
+	
 }
